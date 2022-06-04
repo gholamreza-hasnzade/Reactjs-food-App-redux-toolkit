5a4aa4d68046e75ad32cd83c451986d55dcf7dbf
gholamreza-hasnzade <gholamreza15402@gmail.com>
2022-06-04 20:35:48 +0430

    fixed helemt

---
+++ src/components/helmet/Helmet.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Helmet = ({ title, children }) => {
+
+  document.title = `Food ordering app -${title}`;
+
+  return <div className="w-100">{children}</div>;
+};
+
+export default Helmet;
