47d03f0411c51e94b4290ee66d901402b3454f47
gholamreza-hasnzade <gholamreza15402@gmail.com>
2022-06-04 21:51:01 +0430

    header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,20 +1,80 @@
-import React from 'react'
-import { Container } from 'reactstrap'
- 
-import logo from "../../assets/images/res-logo.png"
+import React, { useRef } from "react";
+
+import { NavLink, Link } from "react-router-dom";
+import { Container } from "reactstrap";
+
+import logo from "../../assets/images/res-logo.png";
+
+import "./header.css";
+
+const nav__links = [
+  {
+    display: "Home",
+    path: "/home",
+  },
+  {
+    display: "Foods",
+    path: "/foods",
+  },
+  {
+    display: "Cart",
+    path: "/cart",
+  },
+  {
+    display: "Contact",
+    path: "/contact",
+  },
+];
 
 const Header = () => {
+  const menuRef = useRef(null);
+  const toggleMenu = () => menuRef.current.classList.toggle("show__menu");
   return (
-    <header className='header'>
+    <header className="header">
       <Container>
-        <div className="nav__wrapper">
-          <div className='logo'>
+        <div className="nav__wrapper d-flex align-items-center justify-content-between">
+          <div className="logo">
             <img src={logo} alt="food logo" />
+            <h5>Tasty Treat</h5>
+          </div>
+          {/* ===== menu ===== */}
+          <div className="navigation" ref={menuRef}  onClick={toggleMenu}>
+            <div className="menu d-flex align-items-center gap-5">
+              {nav__links.map((item, index) => (
+                <NavLink
+                 
+                  key={index}
+                  to={item.path}
+                  className={(navClass) =>
+                    navClass.isActive ? "active__menu" : ""
+                  }
+                >
+                  {item.display}
+                </NavLink>
+              ))}
+            </div>
+          </div>
+          {/* ===== nav right icons ===== */}
+          <div className="nav__right d-flex align-items-center gap-3">
+            <span className="cart__icon">
+              <i className="ri-shopping-basket-line"></i>
+              <span className="cart__badge">2</span>
+            </span>
+
+            <span className="user">
+              <Link to="/login">
+                <i className="ri-user-line"></i>
+              </Link>
+            </span>
+
+            <span className="mobile__menu" onClick={toggleMenu}>
+              <i className="ri-menu-line"></i>
+            </span>
           </div>
         </div>
       </Container>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
