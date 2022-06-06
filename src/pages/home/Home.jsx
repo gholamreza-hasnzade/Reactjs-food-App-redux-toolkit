20b58f3ae73599bd37370840d6b3ecc7081e2150
gholamreza-hasnzade <gholamreza15402@gmail.com>
2022-06-06 21:38:40 +0430

    add hemlat in home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { Col, Container, Row } from 'reactstrap'
+
+import Helmet from '../../components/helmet/Helmet'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Helmet title={"Home"}>
+      <section>
+        <Container>
+          <Row>
+            <Col lg="6" md="6">
+              <div className="hero__content">
+
+              </div>
+            </Col>
+
+            <Col  lg="6" md="6"></Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
   )
 }
 
