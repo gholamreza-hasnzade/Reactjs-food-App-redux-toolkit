6d14469a447b97df7dc7489781472c3923fbaa3f
gholamreza-hasnzade <gholamreza15402@gmail.com>
2022-06-08 10:26:39 +0430

    import imge category

---
+++ src/components/UI/category/Category.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import {Container , Row ,Col} from "reactstrap"
+import { IMAGES } from '../../../constants/content'
+
+
+import "./category.css"
+
+const Category = () => {
+  return (
+    <div>Category</div>
+  )
+}
+
+export default Category
--- src/constants/content/images.constant.js
+++ src/constants/content/images.constant.js
@@ -1,7 +1,16 @@
 import Logo from "../../assets/images/res-logo.png";
-import HeroImg from "../../assets/images/hero.png"
+import HeroImg from "../../assets/images/hero.png";
+
+import CategoryImg01 from "../../assets/images/category-01.png";
+import CategoryImg02 from "../../assets/images/category-02.png";
+import CategoryImg03 from "../../assets/images/category-03.png";
+import CategoryImg04 from "../../assets/images/category-04.png";
 
 export const IMAGES = {
-    LOGO: Logo,
-    HEROIMG: HeroImg
+  LOGO: Logo,
+  HEROIMG: HeroImg,
+  CATEGORYIMG_01: CategoryImg01,
+  CATEGORYIMG_02: CategoryImg02,
+  CATEGORYIMG_03: CategoryImg03,
+  CATEGORYIMG_04: CategoryImg04,
 };
