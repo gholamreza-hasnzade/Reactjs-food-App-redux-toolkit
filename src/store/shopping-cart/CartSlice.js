36881990d1019988de5080780ebc251cc36c017d
gholamreza-hasnzade <gholamreza15402@gmail.com>
2022-06-16 19:20:48 +0430

    [hotfixed] add  to cart

--- src/store/shopping-cart/CartSlice.js
+++ src/store/shopping-cart/CartSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  cartItem: [],
+  cartItems: [],
   totalQuantity: 0,
   totalAmount: 0,
 };
@@ -9,36 +9,37 @@ const cartSlice = createSlice({
   name: "cart",
   initialState: initialState,
 
-  reducer: {
+  reducers: {
     addItem(state, action) {
-      const newItem = action.payload;
-      const existingItem = state.cartItem.find(
-        (item) => item.id === newItem.id
-      );
-      state.totalQuantity++;
-
-      if (!existingItem) {
-        /*  === note: if you use just redux you should not mute
-           state array instead of clone the state  array,
-            but if you use redux toolkit will not a problem
-           because redux toolkit clone the array behind the scene    
-           */
-        state.cartItem.push({
-          id: newItem.id,
-          title: newItem.image01,
-          price: newItem.price,
-          quantity: 1,
-          totalPrice: newItem.price,
-        });
-      } else {
-        existingItem.quantity++;
-        existingItem.totalPrice =
-          Number(existingItem.totalPrice) + Number(existingItem.price);
-      }
-
-      state.totalAmount = state.cartItem.reducer(
-        (total, item) => total + Number(item.price) * Number(item.quantity)
-      );
+        const newItem = action.payload;
+        const existingItem = state.cartItems.find(
+          (item) => item.id === newItem.id
+        );
+        state.totalQuantity++;
+  
+        if (!existingItem) {
+          /*  
+              note: if you use just redux you should not mute
+              state array instead of clone the state  array,
+              but if you use redux toolkit will not a problem
+              because redux toolkit clone the array behind the scene    
+          */
+          state.cartItems.push({
+            id: newItem.id,
+            title: newItem.image01,
+            price: newItem.price,
+            quantity: 1,
+            totalPrice: newItem.price,
+          });
+        } else {
+          existingItem.quantity++;
+          existingItem.totalPrice =
+            Number(existingItem.totalPrice) + Number(existingItem.price);
+        }
+  
+        state.totalAmount = state.cartItems.reduce(
+          (total, item) => total + Number(item.price) * Number(item.quantity)
+        );
     },
   },
 });
