06c732577b81dc44b86d324de7aff7f535685a5b
gholamreza-hasnzade <gholamreza15402@gmail.com>
2022-06-19 22:06:09 +0430

    show add to cart in map

--- src/components/UI/cart/Carts.jsx
+++ src/components/UI/cart/Carts.jsx
@@ -3,44 +3,45 @@ import CartItem from "./CartItem";
 
 import { ListGroup } from "reactstrap";
 import { Link } from "react-router-dom";
-import { useDispatch} from "react-redux";
+
+import { cartUiActions } from "../../../store/shopping-cart/CartUISlice";
+
+import { useDispatch, useSelector } from "react-redux";
 
 import "./cart.css";
-import { cartUiActions } from "../../../store/shopping-cart/CartUISlice";
 
 const Carts = () => {
   const dispatch = useDispatch();
+  const cartProducts = useSelector((state) => state.cart.cartItems);
 
+  console.log(cartProducts);
   const toggleCart = () => {
-    dispatch(cartUiActions.toggle())
-  }
+    dispatch(cartUiActions.toggle());
+  };
   return (
     <div className="cart__container">
       <ListGroup className="cart">
         <div className="cart__close" onClick={toggleCart}>
-            <span><i className="ri-close-line"></i></span>
+          <span>
+            <i className="ri-close-line"></i>
+          </span>
         </div>
 
         <div className="cart__item-list">
-            <CartItem />
-            <CartItem />
-            <CartItem />
-
-            <CartItem />
-            <CartItem />
-            <CartItem />
-
-            <CartItem />
-            <CartItem />
-            <CartItem />
+          {cartProducts.length === 0 ? (
+            <h6 className="text-center mt-5">No item to the cart</h6>
+          ) : (
+            cartProducts.map((item, index) => <CartItem item={item} key={index} />)
+          )}
         </div>
 
-
         <div className="cart__bottom d-flex align-items-center justify-content-between">
-            <h6>Subtotal : <span>$123</span></h6>
-            <button>
-                <Link to={"/checkout"}>Checkout</Link>
-            </button>
+          <h6>
+            Subtotal : <span>$123</span>
+          </h6>
+          <button>
+            <Link to={"/checkout"}>Checkout</Link>
+          </button>
         </div>
       </ListGroup>
     </div>
