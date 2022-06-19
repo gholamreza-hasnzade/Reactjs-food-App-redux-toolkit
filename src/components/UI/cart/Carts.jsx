6cace2b75dab58961e9e423788ef253a81f6d6b5
gholamreza-hasnzade <gholamreza15402@gmail.com>
2022-06-19 21:40:53 +0430

    show and close cart box

--- src/components/UI/cart/Carts.jsx
+++ src/components/UI/cart/Carts.jsx
@@ -3,14 +3,21 @@ import CartItem from "./CartItem";
 
 import { ListGroup } from "reactstrap";
 import { Link } from "react-router-dom";
+import { useDispatch} from "react-redux";
 
 import "./cart.css";
+import { cartUiActions } from "../../../store/shopping-cart/CartUISlice";
 
 const Carts = () => {
+  const dispatch = useDispatch();
+
+  const toggleCart = () => {
+    dispatch(cartUiActions.toggle())
+  }
   return (
     <div className="cart__container">
       <ListGroup className="cart">
-        <div className="cart__close">
+        <div className="cart__close" onClick={toggleCart}>
             <span><i className="ri-close-line"></i></span>
         </div>
 
