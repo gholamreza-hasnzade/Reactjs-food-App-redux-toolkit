f868991984d61afff7cf8f7f32e82b43c57a627b
gholamreza-hasnzade <gholamreza15402@gmail.com>
2022-06-20 22:09:12 +0430

    add item in cart items

--- src/components/UI/cart/CartItem.jsx
+++ src/components/UI/cart/CartItem.jsx
@@ -1,28 +1,41 @@
 import React from "react";
+import { useDispatch } from "react-redux";
 
 import { ListGroupItem } from "reactstrap";
-import { IMAGES } from "../../../constants/content";
+import { cartActions } from "../../../store/shopping-cart/CartSlice";
 
 import "./cart.css";
 
-const CartItem = ({item}) => {
-  const {id , title ,price , image01 , quantity , totalPrice} = item
+const CartItem = ({ item }) => {
+  const { id, title, price, image01, quantity, totalPrice } = item;
 
-  console.log(image01);
+  const dispatch = useDispatch();
+
+  const incrementItem = () => {
+    dispatch(cartActions.addItem({
+      id,
+      title,
+      price,
+      image01
+    }))
+  };
+
   return (
     <ListGroupItem className="border-0 cart__item">
       <div className="cart__item-info d-flex gap-2">
         <img src={image01} alt={title} />
 
-        <div className="cart__product-info d-flex align-items-center w-100
-         justify-content-between gap-4">
+        <div
+          className="cart__product-info d-flex align-items-center w-100
+         justify-content-between gap-4"
+        >
           <div>
             <h6 className="cart__product-title">{title}</h6>
             <p className="d-flex align-items-center gap-5 cart__product-price">
               {quantity}x <span>$ {totalPrice}</span>
             </p>
             <div className="d-flex align-items-center justify-content-between increase__decrease-btn">
-              <span className="increase__btn">
+              <span className="increase__btn" onClick={incrementItem}>
                 <i className="ri-add-line"></i>
               </span>
               <span className="quantity">{quantity}</span>
