263043b4f725c952ba002e27d787f300d3c8551b
gholamreza-hasnzade <gholamreza15402@gmail.com>
2022-06-30 18:30:44 +0430

    cart page cart

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
+import { Container, Row, Col } from "reactstrap";
+
+import { CommonSection } from "../../components/UI";
+import Helmet from "../../components/helmet/Helmet";
 
 const Cart = () => {
   return (
-    <div>Cart</div>
-  )
-}
+    <Helmet title={"cart"}>
+      <CommonSection title={"Your cart"} />
+      <section>
+        <Container>
+          <Row>
+            <Col lg="12">
+              <table className="table table-bordered">
+                <thead>
+                  <tr>
+                    <th>Image</th>
+                    <th>Product Title</th>
+                    <th>Price</th>
+                    <th>Quantity</th>
+                    <th>Delete</th>
+                  </tr>
+                </thead>
+              </table>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
+};
 
-export default Cart
+export default Cart;
