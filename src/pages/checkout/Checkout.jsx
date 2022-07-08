3aa9bf4916b7ea65f1d46402390fd64cc1372bd1
gholamreza-hasnzade <gholamreza15402@gmail.com>
2022-07-08 19:08:15 +0430

    fixed checkout form

--- src/pages/checkout/Checkout.jsx
+++ src/pages/checkout/Checkout.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import { useSelector } from "react-redux";
+
+import Helmet from "../../components/helmet/Helmet";
+import { CommonSection } from "../../components/UI";
 
 const Checkout = () => {
   return (
-    <div>Checkout</div>
-  )
-}
+    <Helmet title={"Checkout"}>
+      <CommonSection title={"Checkout"} />
+      <section>
+        <Container>
+          <Row>
+            <Col lg="8" md="6">
+              <h6 className="mb-4">Shipping Address</h6>
+              <form className="checkout__form">
+                <div className="form__group">
+                  <input type="text" placeholder="Enter your name" />
+                </div>
 
-export default Checkout
+                <div className="form__group">
+                  <input type="email" placeholder="Enter your email" />
+                </div>
+
+                <div className="form__group">
+                  <input type="number" placeholder="Phone number" />
+                </div>
+
+                <div className="form__group">
+                  <input type="text" placeholder="Enter your Country" />
+                </div>
+
+                <div className="form__group">
+                  <input type="text" placeholder="Enter your city" />
+                </div>
+
+                <div className="form__group">
+                  <input type="number" placeholder="Postal code" />
+                </div>
+
+
+                <button className="addToCart__btn">Payment</button>
+              </form>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
+};
+
+export default Checkout;
